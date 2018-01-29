2d4a4eb81e981f0952a1821da5eb782b6d8098e0
Mike Kroutikov <mkroutikov@innodata.com>
2018-01-29 11:01:10 -0500

    playing 1

--- lib1/vars/callMe.groovy
+++ lib1/vars/callMe.groovy
@@ -5,13 +5,10 @@ def call(message) {
 def delegate(Map opt) {
   pipeline {
     agent none
-    parameters {
-      string(name: 'PARAM_1', defaultValue: "${opt.test}", description: 'testing')
-    }
     stages {
       stage ('Root') {
         steps {
-          echo "root! ${params.PARAM_1}"
+          echo "root! ${opt.test}"
         }
       }
     }
